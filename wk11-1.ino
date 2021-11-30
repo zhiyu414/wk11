ff71eccdd1ef3dbfa219e9646d09894baf02a2e0
zhiyu414 <83740867+zhiyu414@users.noreply.github.com>
2021-11-30 11:59:07 +0800

    Add files via upload

---
+++ wk11-1.ino
@@ -0,0 +1,35 @@
+#include <BluetoothSerial.h>
+BluetoothSerial BT;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  BT.begin("rita");//請改名
+  pinMode(12,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (BT.available()) {
+    //讀取藍芽資料
+     char BTdata=BT.read();
+    //顯示在序列視窗
+    Serial.println(BTdata);
+//    if(BTdata='b'){
+//      digitalWrite(12,HIGH);
+////      fflush(stdin);
+//     }
+//    else {
+//      digitalWrite(12,LOW);
+//      }
+    switch(BTdata){
+    case 'b':
+    digitalWrite(12,HIGH);
+    break;
+    case 'a':
+    digitalWrite(12,LOW);
+    break;
+    }
+ }
+   
+  delay(1);
+}
